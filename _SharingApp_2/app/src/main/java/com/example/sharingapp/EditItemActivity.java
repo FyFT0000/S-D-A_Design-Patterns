43338b54a483f73006353cdfc87998c07e8f46fa
FyFT0000 <facundoft@gmail.com>
2023-05-26 12:14:02 +0200

    S2 - C2.1 - 8. Update EditItemActivity to use DeleteItemCommand and EditItemCommand

--- _SharingApp_2/app/src/main/java/com/example/sharingapp/EditItemActivity.java
+++ _SharingApp_2/app/src/main/java/com/example/sharingapp/EditItemActivity.java
@@ -137,8 +137,14 @@ public class EditItemActivity extends AppCompatActivity {
             return;
         }
 
-        item_list.deleteItem(item);
-        item_list.saveItems(context);
+        // Delete item
+        DeleteItemCommand delete_item_command = new DeleteItemCommand(item_list, item,
+                context);
+        delete_item_command.execute();
+        boolean success = delete_item_command.isExecuted();
+        if (!success){
+            return;
+        }
 
         // End EditItemActivity
         Intent intent = new Intent(this, MainActivity.class);
@@ -200,9 +206,14 @@ public class EditItemActivity extends AppCompatActivity {
             updated_item.setBorrower(contact);
         }
 
-        item_list.deleteItem(item);
-        item_list.addItem(updated_item);
-        item_list.saveItems(context);
+        // Edit item
+        EditItemCommand edit_item_command = new EditItemCommand(item_list, item,
+                updated_item, context);
+        edit_item_command.execute();
+        boolean success = edit_item_command.isExecuted();
+        if (!success){
+            return;
+        }
 
         // End EditItemActivity
         Intent intent = new Intent(this, MainActivity.class);
