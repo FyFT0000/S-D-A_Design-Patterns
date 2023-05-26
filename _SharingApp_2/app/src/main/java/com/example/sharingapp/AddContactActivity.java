007263138557c9d00676148337c144158fc10619
FyFT0000 <facundoft@gmail.com>
2023-05-26 13:14:02 +0200

    S2 - C2.1 - 13. Update AddContactActivity to use AddContactCommand

--- _SharingApp_2/app/src/main/java/com/example/sharingapp/AddContactActivity.java
+++ _SharingApp_2/app/src/main/java/com/example/sharingapp/AddContactActivity.java
@@ -53,8 +53,15 @@ public class AddContactActivity extends AppCompatActivity {
         }
 
         Contact contact = new Contact(username_str, email_str, null);
-        contact_list.addContact(contact);
-        contact_list.saveContacts(context);
+
+        // Add contact
+        AddContactCommand addContactCommand = new AddContactCommand(contact_list, contact, context);
+        addContactCommand.execute();
+        boolean success = addContactCommand.isExecuted();
+        if (!success) {
+            return;
+        }
+
         // End AddContactActivity
         finish();
     }
