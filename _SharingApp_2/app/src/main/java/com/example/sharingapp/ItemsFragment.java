463ea8a658fb9a7d7cc7b214c5190b06bf73053e
FyFT0000 <facundoft@gmail.com>
2023-05-29 16:41:19 +0200

    S3 - C2.2 - 11. Update ItemsFragment

--- _SharingApp_2/app/src/main/java/com/example/sharingapp/ItemsFragment.java
+++ _SharingApp_2/app/src/main/java/com/example/sharingapp/ItemsFragment.java
@@ -16,9 +16,10 @@ import java.util.ArrayList;
 /**
  * Superclass of AvailableItemsFragment, BorrowedItemsFragment and AllItemsFragment
  */
-public abstract class ItemsFragment extends Fragment {
+public abstract class ItemsFragment extends Fragment implements Observer {
 
-    ItemList item_list = new ItemList();
+    private ItemList item_list = new ItemList();
+    ItemListController item_list_controller = new ItemListController(item_list);
     View rootView = null;
     private ListView list_view = null;
     private ArrayAdapter<Item> adapter = null;
@@ -26,12 +27,18 @@ public abstract class ItemsFragment extends Fragment {
     private LayoutInflater inflater;
     private ViewGroup container;
     private Context context;
+    private Fragment fragment;
+    private boolean update = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         context = getContext();
-        item_list.loadItems(context);
+
+        // Don't update view yet. Wait until after items have been filtered.
+        item_list_controller.loadItems(context);
+        update = true;
+
         this.inflater = inflater;
         this.container = container;
 
@@ -44,22 +51,20 @@ public abstract class ItemsFragment extends Fragment {
         selected_items = filterItems();
     }
 
-    public void setAdapter(Fragment fragment){
-        adapter = new ItemAdapter(context, selected_items, fragment);
-        list_view.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
+    public void loadItems(Fragment fragment){
+        this.fragment = fragment;
+        item_list_controller.addObserver(this);
+        item_list_controller.loadItems(context);
+    }
 
-        // When item is long clicked, this starts EditItemActivity
+    public void setFragmentOnItemLongClickListener(){
+    // When item is long clicked, this starts EditItemActivity
         list_view.setOnItemLongClickListener(new android.widget.AdapterView.OnItemLongClickListener() {
-
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int pos, long id) {
-
                 Item item = adapter.getItem(pos);
-
-                int meta_pos = item_list.getIndex(item);
+                int meta_pos = item_list_controller.getIndex(item);
                 if (meta_pos >= 0) {
-
                     Intent edit = new Intent(context, EditItemActivity.class);
                     edit.putExtra("position", meta_pos);
                     startActivity(edit);
@@ -75,4 +80,22 @@ public abstract class ItemsFragment extends Fragment {
      */
     public abstract ArrayList<Item> filterItems();
 
+    /**
+     * Called when the activity is destroyed, thus we remove this fragment as an observer
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        item_list_controller.removeObserver(this);
+    }
+    /**
+     * Update the view
+     */
+    public void update(){
+        if (update) {
+            adapter = new ItemAdapter(context, selected_items, fragment);
+            list_view.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+        }
+    }
 }
