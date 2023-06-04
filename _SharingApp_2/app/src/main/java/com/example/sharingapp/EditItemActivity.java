a54f586fd06f7d7c6c38ba2c50432434105b2464
FyFT0000 <facundoft@gmail.com>
2023-06-04 18:25:27 +0200

    S3 - C2.3 - Part 2 - fixing a Long Method anti-pattern, by Implement a validateInput() method in: EditItemActivity

--- _SharingApp_2/app/src/main/java/com/example/sharingapp/EditItemActivity.java
+++ _SharingApp_2/app/src/main/java/com/example/sharingapp/EditItemActivity.java
@@ -122,54 +122,21 @@ public class EditItemActivity extends AppCompatActivity implements Observer {
 
     public void saveItem(View view) {
 
-        String title_str = title.getText().toString();
-        String maker_str = maker.getText().toString();
-        String description_str = description.getText().toString();
-        String length_str = length.getText().toString();
-        String width_str = width.getText().toString();
-        String height_str = height.getText().toString();
-
         Contact contact = null;
         if (!status.isChecked()) {
             String borrower_str = borrower_spinner.getSelectedItem().toString();
             contact = contact_list.getContactByUsername(borrower_str);
         }
 
-        if (title_str.equals("")) {
-            title.setError("Empty field!");
-            return;
-        }
-
-        if (maker_str.equals("")) {
-            maker.setError("Empty field!");
-            return;
-        }
-
-        if (description_str.equals("")) {
-            description.setError("Empty field!");
-            return;
-        }
-
-        if (length_str.equals("")) {
-            length.setError("Empty field!");
-            return;
-        }
-
-        if (width_str.equals("")) {
-            width.setError("Empty field!");
-            return;
-        }
-
-        if (height_str.equals("")) {
-            height.setError("Empty field!");
+        if(!validateInput()) {
             return;
         }
 
         String id = item_controller.getId(); // Reuse the item id
-        Item updated_item = new Item(title_str, maker_str, description_str, image, id);
+        Item updated_item = new Item(title.getText().toString(), maker.getText().toString(), description.getText().toString(), image, id);
 
         ItemController updated_item_controller = new ItemController(updated_item);
-        updated_item_controller.setDimensions(length_str, width_str, height_str);
+        updated_item_controller.setDimensions(length.getText().toString(), width.getText().toString(), height.getText().toString());
 
         boolean checked = status.isChecked();
         if (!checked) {
@@ -186,6 +153,43 @@ public class EditItemActivity extends AppCompatActivity implements Observer {
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public boolean validateInput() {
+
+        String title_str = title.getText().toString();
+        String maker_str = maker.getText().toString();
+        String description_str = description.getText().toString();
+        String length_str = length.getText().toString();
+        String width_str = width.getText().toString();
+        String height_str = height.getText().toString();
+
+        if (title_str.equals("")) {
+            title.setError("Empty field!");
+            return false;
+        }
+        if (maker_str.equals("")) {
+            maker.setError("Empty field!");
+            return false;
+        }
+        if (description_str.equals("")) {
+            description.setError("Empty field!");
+            return false;
+        }
+        if (length_str.equals("")) {
+            length.setError("Empty field!");
+            return false;
+        }
+        if (width_str.equals("")) {
+            width.setError("Empty field!");
+            return false;
+        }
+        if (height_str.equals("")) {
+            height.setError("Empty field!");
+            return false;
+        }
+
+        return true;
     }
 
     /**
